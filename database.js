6aec8b5e033c426563492ca1e940a97bdd9bbe53
adamdturner <adamdturner00@gmail.com>
2024-03-25 20:33:54 -0600

    Update database.js with new collections and createUser

--- database.js
+++ database.js
@@ -1,12 +1,16 @@
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const { v4: uuidv4 } = require('uuid'); // generating unique ID's
 const config = require('./dbConfig.json');
+
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-
 const client = new MongoClient(url);
-const db = client.db('rental');
+const db = client.db('startup');
+const userCollection = db.collection('user');
+const myListsCollection = db.collection('myLists');
+const groupListsCollection = db.collection('groupLists');
 
+// This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
   await client.connect();
   await db.command({ ping: 1 });
@@ -14,3 +18,26 @@ const db = client.db('rental');
   console.log(`Unable to connect to database with ${url} because ${ex.message}`);
   process.exit(1);
 });
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuidv4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
+
